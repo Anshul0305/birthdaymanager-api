077ef41a0552b95e66e49af2c1c9c0518e1154a5
Anshul Shrivastava <anshul.shrivastava123@gmail.com>
2016-05-15 23:40:49 +0100

    Changed message on add fund email

--- scripts/email_templates/add_fund.php
+++ scripts/email_templates/add_fund.php
@@ -1,10 +1,10 @@
 <p>Dear {first_name}, <br>
 
-<p>Your Account has been recharged successfully for team - {team_name} by £{topup}.</p>
+<p>Your fund balance for team - {team_name} has been credited successfully by £{topup}.</p>
 
 <p>Your New Fund Balance is £{fund_balance}.</p>
 
-<p>If you have any questions, Feel free to write us at support@onlinebirthdaymanager.com.</p>
+<p>If you have any questions, feel free to write us at support@onlinebirthdaymanager.com.</p>
 
 <p>Regards, <br>
 Birthday Manager Team</p>
