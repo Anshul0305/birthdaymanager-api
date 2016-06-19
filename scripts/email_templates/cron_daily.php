d145292f2d715dd81d8d298014048003998b1dbe
Anshul Shrivastava <anshul.shrivastava123@gmail.com>
2016-06-19 01:57:27 +0100

    Changed the birthday greeting image

--- scripts/email_templates/cron_daily.php
+++ scripts/email_templates/cron_daily.php
@@ -2,7 +2,7 @@
 <span style="font-family:Cursive;font-size:18px;font-style:italic;font-weight:normal;text-decoration:none;text-transform:capitalize;color:3300CC;">Dear {first_name}, <br>Wish You a Very Happy Birthday! Have a Great Year Ahead.. :)</span>
 <br>
 
-<img src="http://www.epm.com/wp-content/uploads/2014/07/happy_birthday_500.jpg" alt="Happy Birthday">
+<img src="http://www.happybirthdaypix.com/wp-content/uploads/2016/03/happy-birthday-animated-greetings.gif" />
 
 <p>Regards, <br>
 Birthday Manager Team</p>
