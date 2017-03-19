ba7f09b58251013bbd42e3a9ec474fd20b28f68b
Anshul Shrivastava <anshul.shrivastava123@gmail.com>
2017-03-19 11:26:11 +0000

    changed email template for create team

--- scripts/email_templates/create_team.php
+++ scripts/email_templates/create_team.php
@@ -1,13 +1,15 @@
-<p>Dear {first_name} </p>
+<p>Dear {first_name}, </p>
+<br>
 
 <p>You Have Successfully Created Team - {team_name}.</p>
 
-<p> Now start adding new members to your team. To add new member, click on the "Teams" > "View Teams" > "View Details" link and then
+<p> Now start adding members to your team. To add new member, click on the "Teams" > "View Teams" > "View Details" link and then
 <ul><li> Click on "Add Members" button and enter the email id of the member, or</li>
-<li>Copy the "Team Invitation Link" and share the unique link with your friends. Anyone joining with this link will be directly added to your team</li>
+<li>Copy the "Team Invitation Link" and share it with your friends. Anyone joining with this link will be directly added to your team</li>
 </ul>
 
-<p>If you have any other questions, Feel free to write us at hello@onlinebirthdaymanager.com.</p>
+<br>
+<p>If you have any other questions, feel free to write us at hello@onlinebirthdaymanager.com.</p>
 
 <p>Regards, <br></br>
 Birthday Manager Team</p>
